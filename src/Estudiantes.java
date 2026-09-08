6f9494390e34b1d0ec433937fed7cf032c6f9a2a
USUARIO <USUARIO@DESKTOP-VDLVF29>
2026-09-08 15:41:00 -0500

    Agrega un comentario

--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Usuario
- */
 public class Estudiantes {
-    
+    //Este en un comentario
+    //Otro comentario
 }
