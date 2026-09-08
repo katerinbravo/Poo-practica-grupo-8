d2f472b6d03d7566b185b50022d6e36ba0b08373
USUARIO <USUARIO@DESKTOP-VDLVF29>
2026-09-08 16:01:42 -0500

    primero subida

---
+++ src/main/java/com/mycompany/programacionjava/ProgramacionJava.java
@@ -0,0 +1,8 @@
+package com.mycompany.programacionjava;
+
+public class ProgramacionJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/programacionjava/Semana1.java
@@ -0,0 +1,7 @@
+
+package com.mycompany.programacionjava;
+
+
+public class Semana1 {
+    
+}
